5c9d9f586d407f218fe5c31b2962ed60d4b81e47
noyon674 <developernoyon9@gmail.com>
2024-09-16 16:16:21 +0600

    schema is implemented

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 //schema validation for mongodb 
+
+db.createCollection("nobel", { //nobel is a collection name
+    validator: {
+        $jsonSchema: {
+            required: ['name', 'price'], //these are the required fields
+            properties:{
+                name: {
+                    bsonType: 'string', //name is string type
+                    description: 'must be a string and required'
+                },
+                price: {
+                    bsonType: 'number', //price is number type
+                    description: 'must be a number and required'
+                }
+            }
+        }
+    },
+    validationAction: 'error'
+})
