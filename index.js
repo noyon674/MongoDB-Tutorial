183d5f112abaad942253d2eda7d7c647f1cf9d42
noyon674 <developernoyon9@gmail.com>
2024-09-17 23:18:34 +0600

    schema validation done

--- index.js
+++ index.js
@@ -58,3 +58,62 @@ db.runCommand({
     },
     validationAction: 'error'
 })
+
+
+//practice agein
+//create schema
+db.createCollection('nobel', {
+    validator:{
+        $jsonSchema: {
+            required: ['name', 'price'],
+            properties:{
+                name:{
+                    bsonType: 'string',
+                    description: 'must be string and required'
+                },
+                price:{
+                    bsonType: 'number',
+                    description: 'must be number and required'
+                }
+            }
+        }
+    },
+    validationAction: 'error'
+})
+
+//update schema
+db.runCommand({
+    collMod: 'nobel',
+    validator:{
+        $jsonSchema:{
+            required: ['name', 'price', 'authors'],
+            properties: {
+                name: {
+                    bsonType: 'string',
+                    description: 'must be string and required'
+                },
+                price: {
+                    bsonType: 'number',
+                    description: 'must be number and required'
+                },
+                authors:{
+                    bsonType: 'array',
+                    description: 'must be an array and required',
+                    items:{
+                        bsonType: 'object',
+                        required: ['name', 'email'],
+                        properties: {
+                            name: {
+                                bsonType: 'string',
+                            },
+                            email: {
+                                bsonType: 'string'
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    },
+    validationAction: 'error'
+})
